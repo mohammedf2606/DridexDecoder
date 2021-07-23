bdfa752a2814e4eb08916372dd43495f7054eac8
Fahim Mohammed <mohammedf2606@gmail.com>
2021-07-23 05:48:53 -0400

    Add two use cases for xlsb and csv

--- dridextovbs.py
+++ dridextovbs.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+file_loc = input("File path (give full path): ")
+
+extension = file_loc.split(".")[-1]
+print(extension)
+
+if extension == "xlsb":
+    data = pd.read_excel(file_loc, engine="pyxlsb")
+    print(data.shape)
+elif extension == "csv":
+    data = pd.read_csv(file_loc)
+    print(data.shape)
