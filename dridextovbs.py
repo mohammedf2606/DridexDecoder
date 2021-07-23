54fee4047aef53103f7abdec63c30809cb14b097
Fahim Mohammed <mohammedf2606@gmail.com>
2021-07-23 12:06:40 +0100

    Rework to try simpler version

--- dridextovbs.py
+++ dridextovbs.py
@@ -5,9 +5,6 @@ file_loc = input("File path (give full path): ")
 extension = file_loc.split(".")[-1]
 print(extension)
 
-if extension == "xlsb":
-    data = pd.read_excel(file_loc, engine="pyxlsb")
-    print(data.shape)
-elif extension == "csv":
-    data = pd.read_csv(file_loc)
-    print(data.shape)
+data = pd.read_excel(file_loc, engine="pyxlsb")
+print(data.shape)
+
