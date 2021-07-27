e0d0c40c7c396d789b8d99332dc5d9a9f2525bc3
Fahim Mohammed <mohammedf2606@gmail.com>
2021-07-27 07:45:44 -0400

    Add CLI tool features and add offset facility

--- dridextovbs.py
+++ dridextovbs.py
@@ -1,48 +1,68 @@
+import sys
 import pandas as pd
-import numpy as np
-import re
-
-file_loc = "/home/remnux/Downloads/dridex.xlsb"
-# file_loc = "/home/remnux/Downloads/d3bf39bbba7b499e7cc2e20a2986a95164376b2331c1493fd280a467c49d773c.xlsx"
-
-data = pd.DataFrame()
-extension = file_loc.split(".")[-1]
-print(extension)
-
-
-if extension == "xlsb":
-    data = pd.read_excel(file_loc, engine="pyxlsb")
-elif extension == "csv":
-    data = pd.read_csv(file_loc)
-elif extension == "xls":
-    data = pd.read_excel(file_loc, engine="xlrd")
-else:
-    data = pd.read_excel(file_loc)
+from itertools import chain
+import click
 
 
 # Filter rows depending on if there is number
 def filter_row(in_row):
     in_row = in_row[~pd.isna(in_row)]
-    print(in_row)
-
-    # row_string = str(in_row)
-    # row_string = row_string.replace("nan", "").replace("\n", "").replace("[", "").replace("]", "")
-    # split = np.array(row_string.split(" "))
-    # unique_rows = np.unique(split)
-    # without_empty_str = np.delete(unique_rows, np.where(unique_rows == ''))
-    #
-    # pattern = "^'([0-9]+)(.0)?'$"
-    # print(without_empty)
-    # is_match = re.match(pattern, str(without_empty))
-    # if is_match is None:
-    #     return None
-    # print(is_match)
+    return in_row
 
 
-np_array = data.values
-rows_with_nums = []
-for index, row in enumerate(np_array):
-    if filter_row(row) is not None:
-        rows_with_nums.append(row)
-# print(rows_with_nums)
+def process(in_file, offset):
+    file_loc = in_file
 
+    data = pd.DataFrame()
+    extension = file_loc.split(".")[-1]
+
+    if extension == "xlsb":
+        data = pd.read_excel(file_loc, engine="pyxlsb")
+    elif extension == "csv":
+        data = pd.read_csv(file_loc)
+    elif extension == "xls":
+        data = pd.read_excel(file_loc, engine="xlrd")
+    else:
+        data = pd.read_excel(file_loc)
+
+    np_array = data.values
+    empty_rows = []
+    filtered_array = []
+
+    for index, row in enumerate(np_array):
+        filtered = filter_row(row)
+        if filter_row(row).size == 0:
+            empty_rows.append(index)
+        filtered_array.append(filtered.tolist())
+
+    # Join rows together as a single list
+    # Finds the last empty row before the content and selects all rows after that
+    decimal_nums = list(chain(*chain(*[filtered_array[empty_rows[-1] + 1:]])))
+
+    final_script = ""
+    for num in decimal_nums:
+        final_script += chr(int(num) + offset)
+
+    return final_script
+
+
+@click.command()
+@click.option("--in", "-i", "in_file", required=True, type=click.Path(),
+              help="Path to Excel file to be processed.")
+@click.option("--offset", "-f", show_default=True, default=0,
+              help="Offset to add to or subtract from values (specify minus for negative)")
+@click.option("--std-out", "-s", is_flag=True,
+              help="Output to the command line")
+@click.option("--verbose", "-v", is_flag=True,
+              help="Print output to console")
+def main(in_file, offset, std_out, verbose):
+    script = process(in_file, offset)
+    if std_out:
+        print(script)
+        sys.stdout.write(script)
+    elif verbose:
+        print(script)
+
+
+if __name__ == '__main__':
+    main()
